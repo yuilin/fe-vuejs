ea0c6379ffe1fb65a611c5fa3254a44d427c7808
yilin <yuilin@bk.ru>
2019-05-07 09:01:10 +0300

    fixed project filters

--- src/components/projects/Project.vue
+++ src/components/projects/Project.vue
@@ -30,14 +30,41 @@ export default {
     selectedProject () {
       return this.projects.find(project => project.id === Number(this.$route.params.id))
     },
+    filterName () {
+      return this.$store.getters['getFilter14']
+    },
+    filterPosition () {
+      return this.$store.getters['getFilter15']
+    },
+    filterSkill () {
+      return this.$store.getters['getFilter16']
+    },
     data () {
+      let data
       if (this.isSwitched) {
-        return this.parse(this.$store.getters['getEmployees'])
+        data = this.parse(this.$store.getters['getEmployees'])
           .filter(employee => employee.project === undefined && employee.data.position.value !== 'Department Manager')
       } else {
-        return this.parse(this.$store.getters['getEmployees'])
+        data = this.parse(this.$store.getters['getEmployees'])
           .filter(employee => employee.project === this.selectedProject.id)
       }
+      if (this.filterName != null && this.filterName.length > 0) {
+        data = Array.from(new Set(data.filter(data => data.data.name.value.toUpperCase()
+          .search(this.filterName.toUpperCase()) > -1).concat(data.filter(data => data.data.surname.value.toUpperCase()
+          .search(this.filterName.toUpperCase()) > -1))))
+      }
+      if (this.filterPosition != null && this.filterPosition.length > 0) {
+        data = data.filter(data => data.data.position.value.toUpperCase()
+          .search(this.filterPosition.toUpperCase()) > -1)
+      }
+      if (this.filterSkill != null && this.filterSkill.length > 0) {
+        data = data.filter(data => data.data.skills.values.map(
+          (skill) => {
+            return skill.name
+          }
+        ).join(' ').toUpperCase().search(this.filterSkill.toUpperCase()) > -1)
+      }
+      return data
     },
     user () {
       return this.$store.getters['getId']
